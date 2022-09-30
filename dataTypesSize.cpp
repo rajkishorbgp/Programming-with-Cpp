39d041422167a6794d9d7bd6b3aef20274013832
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-01 00:11:12 +0530

    Main git push

---
+++ dataTypesSize.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    cout<<"char"<<endl;
+    cout<<"unsigned char"<<endl;
+    cout<<"signed char"<<endl;
+    cout<<"int"<<endl;
+    cout<<"unsined int"<<endl;
+    cout<<"signed int"<<endl;
+    cout<<"sort int"<<endl;
+    cout<<"unsigned sort int"<<endl;
+    cout<<"signed sort int"<<endl;
+    cout<<"long int"<<endl;
+    cout<<"signed long int"<<endl;
+    cout<<"unsigned long int"<<endl;
+    cout<<"float"<<endl;
+    cout<<"double"<<endl;
+    cout<<"long double"<<endl;
+}
