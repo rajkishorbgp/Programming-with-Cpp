1f505908beb095a14294376e4198d3a8a9a58bf0
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-06 06:33:49 +0530

    Copy contstructor create

--- OOPs/CopyConstructor.cpp
+++ OOPs/CopyConstructor.cpp
@@ -3,24 +3,40 @@
 using namespace std;
 
 class Student{
+
     string name;
     int rollNo;
     char *add;
+
     public:
+
+    Student(){
+        cout<<"Constructor called..."<<endl;
+        add = new char[100];
+    }
+    
     void setName(string name){
         this->name=name;
     }
+
     void setRollNo(int rollNo){
         this->rollNo=rollNo;
     }
+
     string getName(){
         return name;
     }
+    
     int getRollNo(){
         return rollNo;
     }
+
     void setAdd(char add[]){
         strcpy(this->add , add);
+    }
+
+    void printAdd(){
+        cout<<this->add<<endl;
     }
 
 
@@ -34,6 +50,14 @@ class Student{
     //     this->rollNo=st.rollNo;
     // }
 
+    // deep copy  constructor
+    Student(Student& temp){
+        char *ch = new char(strlen(temp.add)+1);
+        strcpy(ch,temp.add);
+        this->add=ch;
+        this->name=temp.name;
+        this->rollNo=temp.rollNo;
+    }   
 
 };
 
@@ -44,13 +68,48 @@ int main()
     s1.setRollNo(6095);
     cout<<"Name :" <<s1.getName()<<endl;
     cout<<"Roll No : " << s1.getRollNo()<<endl;
-    char add[]= "mohanpur";
+    char add[]= "Mohanpur";
     s1.setAdd(add);
      
     Student s2(s1);   // Copy Constructor
     cout<<"Name :" <<s2.getName()<<endl;
     cout<<"Roll No : " <<s2.getRollNo()<<endl;
+    cout<<"s2 add : ";
+    s2.printAdd();
+    s2.setAdd("Mohan");
+    cout<<"s2 add : ";
+    s2.printAdd();
+    cout<<"s1 add : ";
+    s1.printAdd();
+
+    // Copy Assignment  operator
+    s2=s1;
+
+    cout<<"Name :" <<s2.getName()<<endl;
+    cout<<"Roll No : " <<s2.getRollNo()<<endl;
+    cout<<"s2 add : ";
+    s2.printAdd();
+    cout<<"s1 add : ";
+    s1.printAdd();
 
 
     return 0;
 }
+
+/*
+    Output:
+    
+    Constructor called...
+    Name :Raj kishor
+    Roll No : 6095
+    Name :Raj kishor
+    Roll No : 6095
+    s2 add : Mohanpur
+    s2 add : Mohan
+    s1 add : Mohanpur
+    Name :Raj kishor
+    Roll No : 6095
+    s2 add : Mohanpur
+    s1 add : Mohanpur
+    
+*/
