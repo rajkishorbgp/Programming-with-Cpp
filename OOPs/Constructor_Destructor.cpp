ccb62d47ba074e0497630bcd26070cf93f5fa3c4
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-06 06:35:37 +0530

    Constructor and Destructor create

---
+++ OOPs/Constructor_Destructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+
+    string name;
+    int rollNo;
+    public:
+    // Constructor create
+    Student(){
+        cout<<"Constructor called..."<<endl;
+    }
+    // Destructor create
+    ~Student(){
+        cout<< "Destructor called..."<<endl;
+    }
+};
+
+int main()
+{
+    // static
+    Student s1;
+
+    // dynamic
+    Student *s2=new Student();
+
+    Student *s3 = new Student;
+    // manually destructor call
+    
+    return 0;
+}
+
+/* 
+    Output:
+    Constructor called...
+    Destructor called...
+*/
