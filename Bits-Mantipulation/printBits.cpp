395bc553aa57874a112f10e163e3321c03748d43
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2022-10-11 09:59:59 +0530

    update bitPrint

--- Bits-Mantipulation/printBits.cpp
+++ Bits-Mantipulation/printBits.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void printbits(int value)
+void printIntBits(int value) // 32 bits
 {
     for (int i = 31; i >= 0; i--)
     {
@@ -10,13 +10,31 @@ void printbits(int value)
     cout<<endl;
 }
 
+void printCharBit(char value){  // 8 bits
+    for (int i =7; i >= 0; i--)
+    {
+        cout<<((value>> i) & 1);
+    }
+    
+}
+
+void printFloatBit(float value){  // 32 bits
+    for (int i = 31; i <=0; i++)
+    {
+        cout<<((value>> i) & 1);
+    }
+}
+
 int main()
 {
-    int num = 10;
-    unsigned int num1 = 0;
+    int num = -10;
+    unsigned int num1 = 10;
     
-    printbits(num);
-    printbits(num1);
+    printIntBits(num);
+    printIntBits(num1);
 
+    printCharBit('a');
+
+    printFloatBit(5.2);
     return 0;
 }
