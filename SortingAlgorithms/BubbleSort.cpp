07656f69b93acde62dd1c709f205380345f6eb7c
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-01-31 17:45:16 +0530

    BubbleSort

---
+++ SortingAlgorithms/BubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(int);
+    bubbleSort(arr, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+/*
+    Output:
+    1 2 3 4 5 6 7 8 9
+*/
